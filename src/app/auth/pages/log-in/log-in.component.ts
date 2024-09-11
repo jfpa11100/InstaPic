63fe6017ba30bfc1d5c051ed3879aa59448a1ce3
jfpa11100 <felipepala1000@gmail.com>
2024-09-11 00:10:55 -0500

    implementation of the log In functionality and first steps of register form

--- src/app/auth/pages/log-in/log-in.component.ts
+++ src/app/auth/pages/log-in/log-in.component.ts
@@ -1,21 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { User } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   template: `
     <div class="login-box">
       <h2>Iniciar Sesión</h2>
-      <input type="text" id="username" placeholder="Nombre de usuario"/>
-      <input type="password" id="password" placeholder="Contraseña"/>
-      <a [routerLink]="['/home']" class="primary">Ingresar</a>
+      <form [formGroup]="loginForm">
+        <input type="text" formControlName="username" id="username" placeholder="Nombre de usuario"/>
+        <input type="password" formControlName="password" id="password" placeholder="Contraseña"/>
+      </form>
+      <a (click)="onLogin()" class="primary">Ingresar</a>
       <a [routerLink]="['/sign-up']" href="sign-up.html" class="secondary">Registrarse</a>
-  </div>
+    </div>
   `,
   styleUrl: './log-in.component.css'
 })
-export class LogInComponent {
+export class LogInComponent{
+  
+  user: User = {
+    username: '',
+    password: ''  
+  }
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router ){
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']  
+    })    
+  }
+
+  onLogin():void{
+    console.log('onLogin')
+    let username = this.loginForm.value.username;
+    let password = this.loginForm.value.password;
+
+    if (!username || !password) {
+      alert("Debe diligenciar los campos")
+      return;
+    }
+    const storedPassword = localStorage.getItem(username.toLowerCase())
+
+    if (storedPassword === null){
+      alert("Usuario no registrado")
+      return
+    }
+    else if (password === storedPassword){
+      this.router.navigateByUrl('/home')  
+    }
+    else {
+      alert("Contraseña incorrecta")
+    }
+  }
+
+
 
 }
--- src/app/auth/pages/sign-up/sign-up.component.ts
+++ src/app/auth/pages/sign-up/sign-up.component.ts
@@ -1,23 +1,55 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { User } from '../../interfaces/user.interface';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   template: `
     <div class="login-box">
         <h2>Registrarse</h2>
-        <input type="text" id="username" placeholder="Nombre de usuario"/>
-        <input type="email" id="email" placeholder="Email"/>
-        <input type="pasword" id="password" placeholder="Contraseña"/>
-        <input type="password" id="re-password" placeholder="Confirmar contraseña"/>
-        <a [routerLink]="['/home']" class="primary">Registrarse</a>
+        <form [formGroup]="registerForm">
+          <input type="text" formControlName="username" id="username" placeholder="Nombre de usuario"/>
+          <input type="email" formControlName="email" id="email" placeholder="Email"/>
+          <input type="pasword" formControlName="password" id="password" placeholder="Contraseña"/>
+          <input type="password" formControlName="rePassword" id="re-password" placeholder="Confirmar contraseña"/>
+        </form>
+        <a (click)="onRegister()" class="primary">Registrarse</a>
         <a [routerLink]="['/login']" href="index.html" class="secondary">¿Ya tiene cuenta?</a>
     </div>
   `,
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  
+  user: User = {
+    username: '',
+    password: ''  
+  }
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router ){
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(15)]],  
+      rePassword: ['', [Validators.required]]  
+    })    
+  }
+
+  onRegister():void{
+    console.log('onRegister')
+    let username = this.registerForm.value.username;
+    let email = this.registerForm.value.email;
+    let password = this.registerForm.value.password;
+    let rePassword = this.registerForm.value.rePassword;
+
+    if (!username || !password) {
+      alert("Debe diligenciar los campos")
+      return;
+    }
+    //Controlar ingreso al localStorage
+  }
 }
